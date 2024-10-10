d124b6aa2b7265b8178542a7c7fe7dbc9e987a44
99webdesign <83107749+99webdesign@users.noreply.github.com>
2024-10-10 15:27:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
     const blogs = [
         { title: "How Website Design Helps Business", url: "https://99webdesign.net/blog" },
-        { title: "Learn WordPress for Free", url: "https://riteshacademy.com/blog" },
-        { title: "Best Practices of SEO in Website Designing", url: "https://riteshacademy.com/blog" }
+        { title: "Best Practices in Website SEO", url: "https://99webdesign.net/blog" },
+        { title: "Why Responsive Design Matters", url: "https://99webdesign.net/blog" },
+        { title: "Advanced Digital Marketing Quiz", url: "https://riteshacademy.com/blog" },
+        { title: "Digital Marketing Strategies", url: "https://riteshacademy.com/blog" },
+        { title: "Learn WordPress Basics", url: "https://riteshacademy.com/blog" },
+        { title: "Why Blogging is Important", url: "https://riteshacademy.com/blog" }
     ];
 
     const blogList = document.getElementById('blog-list');
